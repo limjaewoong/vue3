962ff11469574a200b604da1b2e183a30fe50751
ljw <ijujjang6@gmail.com>
2024-12-22 19:45:32 +0900

    111

---
+++ src/assets/scripts/movies.js
@@ -0,0 +1,38 @@
+const libs = {
+    data : [
+        {
+            title: '노랑'
+            , year: 2023
+            , category: '액션, 드라마'
+            , textRed: 'color:red'
+            , like: 0
+            , img_path: '/assets/1_1.jpg'
+        }
+        , {
+            title: '빨간'
+            , year: 2024
+            , category: '액션, 드라마'
+            , textRed: 'color:red'
+            , like: 0
+            , img_path: '/assets/1_2.jpg'
+        }
+        , {
+            title: '파랑'
+            , year: 2024
+            , category: '액션, 드라마'
+            , textRed: 'color:blue'
+            , like: 0
+            , img_path: '/assets/1_3.jpg'
+        }
+        , {
+            title: '그린'
+            , year: 2024
+            , category: '액션, 드라마'
+            , textRed: 'color:green'
+            , like: 0
+            , img_path: '/assets/1_4.jpg'
+        }
+    ]
+}
+
+export default libs;
